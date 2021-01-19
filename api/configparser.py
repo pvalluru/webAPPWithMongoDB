a6c043076dee0bc707629a2aa983f546929790b4
Prashanth <vpr.pra@gmail.com>
2021-01-19 17:55:30 +0530

    spelling mistake

--- api/configparser.py
+++ api/configparser.py
@@ -1,11 +1,11 @@
 import yaml
 
 def getTokenExpairTime():
-    default_TOKEN_EXPAIR_TIME = 15
+    default_TOKEN_EXPIRY_TIME = 15
     try:
         with open("Config/config.yml", "r") as ymlfile:
             cfgfile = yaml.load(ymlfile)
-        return cfgfile['TOKEN_EXPAIR_TIME'] if cfgfile['TOKEN_EXPAIR_TIME'] else default_TOKEN_EXPAIR_TIME
+        return cfgfile['TOKEN_EXPIRY_TIME'] if cfgfile['TOKEN_EXPIRY_TIME'] else default_TOKEN_EXPIRY_TIME
     except Exception as e:
         print("Caught Exception using default fetch time.")
-        return default_TOKEN_EXPAIR_TIME
+        return default_TOKEN_EXPIRY_TIME
